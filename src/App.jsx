b589c6f89cbd4a8434fa4fb803e39b73e9a423b3
Artem Matvieientsev <tmatveentsev@gmail.com>
2025-01-11 00:48:25 +0100

    styles module

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Header from "./components/Header/Header";
 import User from "./components/User/User";
-import styles from "./components/User/User.module.css";
+import style from "./App.module.css";
 
 import userData from "./assets/users.json";
 
@@ -9,11 +9,15 @@ const App = () => {
     <>
       <Header />
       <h2>Lesson 2. Styles</h2>
-      <ul className={styles.wrapper}>
+      <ul>
         {userData.map((user) => (
           <User key={user.id} {...user} />
         ))}
       </ul>
+      <div className={style.wrapper}>
+        <div className={style.boxOne}>First Box</div>
+        <div className={style.boxTwo}>Second Box</div>
+      </div>
     </>
   );
 };
--- src/Components/User/User.jsx
+++ src/Components/User/User.jsx
@@ -1,11 +1,20 @@
+import clsx from "clsx";
 import style from "./User.module.css";
 
-const User = ({ name, balance, email }) => {
+const User = ({ name, balance, email, isOpenToWork }) => {
   return (
     <li className={style.card}>
       <h2>{name}</h2>
       <p>{balance}</p>
       <p>{email}</p>
+      <p
+        className={clsx(
+          style.status,
+          isOpenToWork ? style.greenAccent : style.redAccent
+        )}
+      >
+        {isOpenToWork ? "Looking for a job" : "Not looking for work"}
+      </p>
       <button>Delete</button>
     </li>
   );
